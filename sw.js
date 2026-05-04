0a854ae69623f1d9e033fb244191f9e145761d6e
mectatel73-ux <mectatel73@gmail.com>
2026-05-05 00:37:56 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'warehouse-guide-v1';
+const urlsToCache = [
+  '/warehouse-guide/',
+  '/warehouse-guide/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
